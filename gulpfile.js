fe9a58cad866e81e064808e66e579c504df915a3
Ben Zörb <ben@sommerlaune.com>
2016-11-07 07:38:17 +0100

    temporary disable grunt tests

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('pre-test', function () {
 gulp.task('test', ['pre-test'], function (cb) {
     var mochaErr;
 
-    gulp.src('test/*.js')
+    gulp.src('test/gulp.js')
         .pipe(plumber())
         .pipe(mocha({reporter: 'spec'}))
         .on('error', function (err) {
