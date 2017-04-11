beeb2e5b3d7e462313d465b573b65b606275ca3b
Ray Foss <ray@vblip.com>
2017-04-11 07:52:30 -0500

    This whitelists what should go to bs/webpack, sends everything else to php-proxy-middleware. allowing for symfony to handle json, xml, arbitrary extensions and 404 pages.

--- generators/buildtool/gulp/templates/gulp/helper/utils.js
+++ generators/buildtool/gulp/templates/gulp/helper/utils.js
@@ -3,11 +3,14 @@ import path from 'path';
 import php from 'php-proxy-middleware';
 import {flatten, first} from 'lodash';
 import {getenv} from './env';
+import fs from 'fs';
 
 export const paths = {
     app: 'app/Resources/public',
     dist: 'web'
 };
+
+export const bsRootPaths = ['.tmp', paths.app, './', 'node_modules', paths.dist];
 
 function getMiddleware() {
     const env = getenv('env');
@@ -27,13 +30,28 @@ export function phpMiddleware() {
     var middleware = getMiddleware();
 
     return function (req, res, next) {
-        var obj = parseurl(req);
-        if (!/\.\w{2,}$/.test(obj.pathname) || /\.php/.test(obj.pathname)) {
-            middleware(req, res, next);
-        } else {
+        var url = parseurl(req);
+        if (
+            /^\/__webpack_hmr$/.test(url.pathname) ||
+            /^\/browser-sync\//.test(url.pathname) ||
+            isBsFile(url.pathname) // real file to bs
+        ) {
             next();
+        } else {
+            middleware(req, res, next);
         }
     };
+}
+
+function isBsFile (path) {
+    for(var i=0; i < bsRootPaths.length ; i++) {
+        if (fs.existsSync(bsRootPaths[i] + path)
+            && fs.statSync(bsRootPaths[i] + path).isFile()
+        ) { // it's a file!
+            return true;
+        }
+    }
+    return false;
 }
 
 export function prefixPaths(source, ...rest) {
